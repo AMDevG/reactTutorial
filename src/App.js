254c76684a9eb5a0deeb3d5c3ec9d58e12f4f41c
John Berry <JBerry@cihedging.com>
2021-09-17 13:39:53 -0500

    Begin Ch. 39

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 function App() {
+
+  const expenses = [
+
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020,7,13),
+    },
+
+    {
+      id: 'e2',
+      title: 'OnlyFans',
+      amount: 94.12,
+      date: new Date(2021,1,5),
+    },
+    {
+      id: 'e3',
+      title: 'Insurance',
+      amount: 110.02,
+      date: new Date(2021,6,15),
+    },
+  ];
+
+
   return (
     <div> 
-        <h2>Let's get started!</h2>
-        <ExpenseItem></ExpenseItem>
-        <ExpenseItem></ExpenseItem>
-        <ExpenseItem></ExpenseItem>
+      <Expenses items={expenses} />
     </div>
   );
 }
